17196c6ef91340cd3ac784a634b105f53dc0923e
Deependra-Das <iamdeep75@gmail.com>
2024-09-24 14:40:18 +0530

    Added Code to draw Background Image
    - Create the header & source files for the Level Service, Model, View & Controller
    - Added the code in Level controller & View to draw background image
    - Updated the Service Locator for Level Service

--- Array-Jumper/source/Global/ServiceLocator.cpp
+++ Array-Jumper/source/Global/ServiceLocator.cpp
@@ -2,6 +2,7 @@
 #include "../../header/Main/GameService.h"
 #include "../../header/Event/EventService.h"
 #include "../../header/Player/PlayerService.h"
+#include "../../header/Level/LevelService.h"
 
 namespace Global
 {
@@ -11,6 +12,7 @@ namespace Global
 	using namespace UI;
 	using namespace Main;
 	using namespace Player;
+	using namespace Level;
 
 	ServiceLocator::ServiceLocator()
 	{
@@ -31,6 +33,7 @@ namespace Global
 		sound_service = new SoundService();
 		ui_service = new UIService();
 		player_service = new PlayerService();
+		level_service = new LevelService();
 	}
 
 	void ServiceLocator::initialize()
@@ -40,6 +43,7 @@ namespace Global
 		sound_service->initialize();
 		ui_service->initialize();
 		player_service->initialize();
+		level_service->initialize();
 	}
 
 	void ServiceLocator::update()
@@ -50,6 +54,7 @@ namespace Global
 
 		if (GameService::getGameState() == GameState::GAMEPLAY)
 		{
+			level_service->update();
 			player_service->update();
 		}
 	}
@@ -61,6 +66,7 @@ namespace Global
 
 		if (GameService::getGameState() == GameState::GAMEPLAY)
 		{
+			level_service->render();
 			player_service->render();
 		}
 	}
@@ -72,6 +78,7 @@ namespace Global
 		delete(sound_service);
 		delete(ui_service);
 		delete(player_service);
+		delete(level_service);
 	}
 
 	ServiceLocator* ServiceLocator::getInstance()
@@ -89,4 +96,6 @@ namespace Global
 	UIService* ServiceLocator::getUIService() { return ui_service; }
 
 	PlayerService* ServiceLocator::getPlayerService() { return player_service; }
+
+	LevelService* ServiceLocator::getLevelService() { return level_service; }
 }
