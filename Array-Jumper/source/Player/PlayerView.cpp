27d82ba48774e57d26a127b74e6851ac05d2870d
Deependra-Das <iamdeep75@gmail.com>
2024-09-24 10:39:08 +0530

    Added Code to render player
    - Created header & source files for Player Model & View
    - Added Code to store player data & render player image

---
+++ Array-Jumper/source/Player/PlayerView.cpp
@@ -0,0 +1,62 @@
+#include "../../header/Player/PlayerView.h"
+#include "../../header/Global/Config.h"
+#include "../../header/Global/ServiceLocator.h"
+
+namespace Player
+{
+	using namespace Global;
+	using namespace UI::UIElement;
+
+	PlayerView::PlayerView()
+	{
+		game_window = nullptr;
+		player_image = new ImageView();
+
+	}
+
+	PlayerView::~PlayerView() {}
+
+	void PlayerView::initialize()
+	{
+		game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+		loadPlayer();
+	}
+
+	void PlayerView::update()
+	{
+
+	}
+
+	void PlayerView::render()
+	{
+
+	}
+
+	void PlayerView::loadPlayer()
+	{
+		calculatePlayerDimensions();
+		initializePlayerImage();
+	}
+
+	void PlayerView::calculatePlayerDimensions()
+	{
+		player_width = 1000.f;
+		player_height = 1000.f;
+	}
+
+	sf::Vector2f PlayerView::calculatePlayerPosition()
+	{
+		return sf::Vector2f(0, 0);
+	}
+
+	void PlayerView::initializePlayerImage()
+	{
+		player_image->initialize(Config::character_texture_path, player_width, player_height, sf::Vector2f(0, 0));
+	}
+
+	void PlayerView::drawPlayer()
+	{
+		player_image->render();
+	}
+
+}
