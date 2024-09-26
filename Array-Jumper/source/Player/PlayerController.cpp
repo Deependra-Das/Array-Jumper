d837ac218c65b1e6bb2e4e20e5e35822fe795c7e
Deependra-Das <iamdeep75@gmail.com>
2024-09-26 23:58:13 +0530

    Implemented Player Movement
    - Added header file for MovementDirection & added the enum for direction
    - Updated the PlayerController & added readInput() function to take input using event service & move() & isPositionInBound() function to pocess the input & update the player position based on the direction & input

--- Array-Jumper/source/Player/PlayerController.cpp
+++ Array-Jumper/source/Player/PlayerController.cpp
@@ -1,9 +1,16 @@
 #include "../../header/Player/PlayerController.h"
 #include "../../header/Player/PlayerModel.h"
 #include "../../header/Player/PlayerView.h"
+#include "../../header/Player/MovementDirection.h"
+#include "../../header/Global/ServiceLocator.h"
+#include "../../header/Sound/SoundService.h"
 
 namespace Player
 {
+	using namespace Level;
+	using namespace Global;
+	using namespace Sound;
+
 	PlayerController::PlayerController()
 	{
 		player_model = new PlayerModel();
@@ -17,6 +24,7 @@ namespace Player
 
 	void PlayerController::initialize()
 	{
+		event_service=ServiceLocator::getInstance()->getEventService();
 		player_model->initialize();
 		player_view->initialize();
 	}
@@ -24,6 +32,7 @@ namespace Player
 	void PlayerController::update()
 	{
 		player_view->update();
+		readInput();
 	}
 
 	void PlayerController::render()
@@ -50,5 +59,54 @@ namespace Player
 	int PlayerController::getCurrentPosition()
 	{
 		return player_model->getCurrentPosition();
+	}
+
+	void PlayerController::move(MovementDirection direction)
+	{
+		int steps, targetPosition;
+		switch (direction)
+		{
+		case MovementDirection::FORWARD:
+			steps = 1;
+			break;
+		case MovementDirection::BACKWARD:
+			steps = -1;
+			break;
+		default:
+			steps = 0;
+			break;
+		}
+
+		targetPosition = player_model->getCurrentPosition() + steps;
+
+		if (!isPositionInBound(targetPosition))
+		{
+			return;
+		}
+
+		player_model->setCurrentPosition(targetPosition);
+		ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::MOVE);
+	}
+
+	bool PlayerController::isPositionInBound(int targetPosition)
+	{
+		if (targetPosition >= 0 && targetPosition < LevelData::number_of_boxes)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	void PlayerController::readInput()
+	{
+		if (event_service->pressedRightArrowKey() || event_service->pressedDKey())
+		{
+			move(MovementDirection::FORWARD);
+		}
+		if (event_service->pressedLeftArrowKey() || event_service->pressedAKey())
+		{
+			move(MovementDirection::BACKWARD);
+		}
 	}
 }
